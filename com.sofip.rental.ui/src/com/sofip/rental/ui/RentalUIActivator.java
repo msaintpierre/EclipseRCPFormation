bff0ebad389e2043c732136a9560ab51d26c379f
localadmin <localadmin@localadmin-VirtualBox>
2015-03-06 12:31:13 +0100

    Points d'extention

--- com.sofip.rental.ui/src/com/sofip/rental/ui/RentalUIActivator.java
+++ com.sofip.rental.ui/src/com/sofip/rental/ui/RentalUIActivator.java
@@ -1,5 +1,8 @@
 package com.sofip.rental.ui;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -31,6 +34,19 @@ public class RentalUIActivator extends AbstractUIPlugin implements RentalUIConst
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		displayViewExtensions();
+	}
+
+	
+	
+	private void displayViewExtensions() {
+		IExtensionRegistry reg = Platform.getExtensionRegistry();
+		for (IConfigurationElement e : reg.getConfigurationElementsFor("org.eclipse.ui.views"))
+		{
+			if (e.getName().equals("view")) {
+			System.out.println("Vue : " + e.getAttribute("name") + " - Plug-in : " + e.getNamespaceIdentifier());
+			}
+		}
 	}
 
 	/*
