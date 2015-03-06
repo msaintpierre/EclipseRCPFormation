e9b33406fd031ce30ca6b84f2768f2a9dd878914
localadmin <localadmin@localadmin-VirtualBox>
2015-03-06 17:43:53 +0100

    rap

---
+++ com.sofip.rental.rcp/src/com/sofip/rental/rcp/ApplicationActionBarAdvisor.java
@@ -0,0 +1,43 @@
+package com.sofip.rental.rcp;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.ui.actions.ActionFactory;
+
+public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+	private IAction quitAction;
+	private IAction preferencesAction;
+
+    public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+        super(configurer);
+    }
+
+    protected void makeActions(IWorkbenchWindow window) {
+    	{
+    		quitAction = ActionFactory.QUIT.create(window);
+    		register(quitAction);
+    	}
+    	{
+    		preferencesAction = ActionFactory.PREFERENCES.create(window);
+    		register(preferencesAction);
+    	}
+    }
+
+    protected void fillMenuBar(IMenuManager menuBar) {
+    	
+    	MenuManager menuManager = new MenuManager("File");
+    	menuBar.add(menuManager);
+    	menuManager.add(quitAction);
+    	menuBar.add(new Separator("additions"));
+    	
+    	MenuManager menuManager_1 = new MenuManager("Window");
+    	menuBar.add(menuManager_1);
+    	menuManager_1.add(preferencesAction);
+    }
+    
+}
---
+++ com.sofip.rental.rcp/src/com/sofip/rental/rcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package com.sofip.rental.rcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+import com.sofip.rental.ui.perspective.RentalPerspective;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return RentalPerspective.ID;
+	}
+}
